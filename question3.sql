a86b5b88d1b12b08bf25aa7a141f1cb10d8862fc
Mageshkanna <36483110+MageshKanna@users.noreply.github.com>
2023-02-17 16:54:03 +0530

    Create question3.sql

---
+++ question3.sql
@@ -0,0 +1,126 @@
+
+INSERT INTO `abhi_question` (`que_id`, `que_desc`, `ans1`, `ans2`, `ans3`, `ans4`, `true_ans`) VALUES
+(1,'Which is the correct sequence of 
+operation to be performed when using the fire?','Pull,Aim Squeeze, 
+Sweep','Pull,Aim, Sweep,
+Squeeze','Push, Arrnge, 
+Squeeze,Sweep','Push, Arrange,
+Sweep, Sequence',1,1),
+	(2,'Which Personal Protective Equipment 
+is used for the protection from fumes?','Apron','Goggles','Ear mask','Nose mask',4,1),
+	(3,'What is the full form of BIS?','Board of Indian 
+Standard','Bureau of Indian 
+Standard','Board of 
+International 
+Standard','Bureau of 
+Internatioal Standard',2,1),
+	(4,'Which plier isused for makin wire 
+hooks and loops?','Flat nose plier','Long nose plier','Round nose 
+plier','Diagonal cutting plier',3,1),
+	(5,'What is the fullform of XLPE Cable?','Cross Line 
+PolyEthylene','Xess Line Phase 
+Earthing','Crss Linked 
+PolyEthylene','Excess length 
+paper and Ebonite',3,1),
+	(6,'How many electrons are there in 
+the valence shell of a copper atom','1','2','8','18',1,1),
+	(7,'What is the unit of Quantity of electricity?','Coulomb','Volt/second','Ampere/second','EMF',1,2),
+	(8,'_________ Use caution when working 
+near electricity','Always','Never','Rarely','Sometimes',1,2),
+	(9,'__________ is the flow of electrons 
+around the circuit','EMF','Electricity','Power','Voltage',2,2),
+	(10,'_________ all electrical equipment before use','Clean','Inspect','Label','Organize',2,2),
+	(11,'Which of the following does 
+not change in transformer','Current','Voltage','Frequency','Magnetic flux',2,2),
+	(12,'A transformer core is 
+laminated to__________','Reduce hysteresis 
+losses','Reduce eddy 
+current losses','Reduce 
+copper losses','Reduce all above 
+losses',2,2),
+	(13,'A transformer transforms','Voltage','Current','Power','Frequency',3,2),
+	(14,'Primary winding of a Transformer','is always a low 
+voltage winding','is always a high 
+voltage reading','Could either be a 
+low voltage or high 
+voltage winding','Equal to input voltage',3,2),
+	(15,'A common method of 
+cooling in power transformer is','Natural air cooling','air blast cooling','Oil cooling','Water cooling',3,2),
+	(16,'Which of the following are 
+physical devices of a computer','Hardware','System software','Software','package',1,2),
+	(17,'Which of the following unit is 
+responsible for converting the 
+data received from the user into a 
+computer understandable format','Output unit','Input unit','Memory unit','Airthmetic and 
+logic unit',2,2),
+	(18,'Which of the following is the 
+brain of the computer','Central Processing 
+Unit','Memory','Airthmetic and 
+logic unit','Control Unit',1,2),
+	(19,'Which of the following service 
+allows a user to login into another 
+computer somewhere on the internet','E- mail','UserNet','Telnet','FTP',3,2),
+	(20,'WWW stands for','World Whole Web','Wide World Web','Web World Wide','World Wide Web',4,2),
+	(21,'The programmable logic 
+controllers are used in','Manufacturing','Automation','Both A&B','None',3,2),
+	(22,'The PLC’s can be programmed in','Central Processing 
+Unit','Ladder logic,Sequential 
+function chart,Functional 
+block diagram','Memory','C Language',2,2),
+	(23,'Before PLC’s was created 
+many industries used','Relays','Capacitors','resistors','Inductors',1,2),
+	(24,'Which is the first PLC model','PLC 084','PLC 085','PLC 086','PLC 087',1,2),
+	(25,'CCTV cameras is an example 
+for ________ automation.','Building Automation','Office Automation','Industrial Automation','Scientific Automation',2,2),
+	(26,'Who invented the Programmable 
+Logic Controller (PLC)?','Jonas Wenstrom','Dick Morley','Thomas Davenport','Michel Faraday',2,2),
+	(27,'The advantages of PLC are','Cost is high','Easy maintenance, 
+Reliability is high, 
+small in size','High maintenance','Less Reliability',2,2),
+	(28,'________is an example for light automation?','Rocket launching','Street solar lightening','Automated bottle 
+filling stations','Smoke detectors',2,2),
+	(29,'The PLC internally operates, stores, 
+and calculates the value in _____','Binary format','Decimal format','Octal format','Hexadecimal format',1,2),
+	(30,'Which one is the correct 
+sequence for PLC operation?','Self-test, input scan, 
+logic scan, output scan','Self-test, logic scan, 
+output scan, input scan','Self-test, input scan, 
+output scan, logic scan','logic scan, output 
+scan, input scan',1,2),
+	(31,'The DC and AC relays works on
+ __________ principle','. Motors','Electromagnetic induction','Electromechanical components','Generators',2,2),
+	(32,'In PLC the user can write the 
+programs with the help of ________','Optical isolation','. Sensing devices','Programming devices','Ladder diagram',3,2),
+	(33,'The Relay operates on','Low power, Control circuit 
+of very high power','when supply is off','At single phase','High power',1,2),
+	(34,'Which one is the oldest 
+programming language','Ladder logic','Function Block diagram','Structured text programming','Instruction list',1,2),
+	(35,'PLC operates on the following signals','Digital','Impulse','Analog','Frequency',1,2),
+	(36,'The SCADA system used to_________','Monitor','Control','Both A and B','Calculate',3,2),
+	(37,'The standard form of MTU is________','Master Terminal Unit','Main Terminal Unit','Main Time Unit','Master time unit',1,2),
+	(38,'The SCADA system performs','Data presentation, data aquisition, Networked data communication','Only communication','Networking','Only data representation',1,2),
+	(39,'The first generation 
+SCADA systems were developed 
+or designed in_________','1960','1970','1980','1975',1,2),
+	(40,'How many type of control systems 
+are there for SCADA systems','One','Two','Three','Four',2,2),
+	(41,'What are the types of SCADA systems?','Monolithic, Distributed','Monolithic, Networked','Monolithic, Distributed, 
+Networked','Monolithic',3,2),
+	(42,'The standard form of MMI is','Main Machine Interface','Master Machine Interface','Man Machine Interface','Main Master Interface',3,2),
+	(43,'The standard form of RTU is','Remote Terminal Unit','Reverse Terminal Unit','Reverse Time Unit','Remote Reverse Unit',1,2),
+	(44,'What are the components of 
+traditional SCADA system','Monitor','Both C and D','Remote Telemetry Unit,
+Central station','Communication system',2,2),
+	(45,'The Heart of the SCADA system is','CPU','PLC','Relays','I/ O task',4,2),
+	(46,'Which is not a principle of 
+user interface design','Compatibility','Predictability','Forgiveness','Robustness',4,2),
+	(47,'Pointing and selection controls such 
+as the mouse ot trackball enables','Faster learning','Low typing requirements','Faster user & problem - 
+solving','Replaces national 
+language',2,2),
+	(48,'What is the speed of proof 
+reading text on paper?','100 words/ minute','200 words/ minute','400 words/ minute','300 words/ minute',2,2),
+	(49,'When users are involved in complex
+ tasks, the demand on ________ can be 
+significant','Short term memory','Shortcuts','Objects that appear on the screen','Design architecture',1,2),
+	(50,'____________ minimises errors','Affordable','Visibility','Desktop metaphors','Mouse',3,2);
